55dcde0ba857a86405f701aafe3db73185d222d4
Isabela <guerreroisabela243@gmail.com>
2026-05-05 10:41:19 -0500

    administracion de rutas y paraderos

--- src/nodos/dto/create-nodo.dto.ts
+++ src/nodos/dto/create-nodo.dto.ts
@@ -1,15 +1,16 @@
-import { IsInt, IsNotEmpty, Min } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, Min } from 'class-validator';
 import { Ruta } from 'src/rutas/entities/ruta.entity';
 import { Paradero } from 'src/paradero/entities/paradero.entity';
 
 export class CreateNodoDto {
   @IsInt()
   @Min(1)
+  @IsOptional()
   orden?: number;
 
   @IsNotEmpty()
-  ruta?: Ruta;
+  ruta?: Ruta | { id: number };
 
   @IsNotEmpty()
-  paradero?: Paradero;
+  paradero?: Paradero | { id: number };
 }
