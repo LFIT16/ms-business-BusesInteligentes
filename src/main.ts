5727e858862f715521c4ebb567d46d1920525e79
LFIT16 <ibarratucano@gmail.com>
2026-05-05 20:41:49 -0500

    Imagen de bus

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
--- src/migrations/1778029590088-InitCinemaSchema.ts
+++ src/migrations/1778029590088-InitCinemaSchema.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitCinemaSchema1778004077881 implements MigrationInterface {
-    name = 'InitCinemaSchema1778004077881'
+export class InitCinemaSchema1778029590088 implements MigrationInterface {
+    name = 'InitCinemaSchema1778029590088'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`paraderos\` (\`id\` int NOT NULL AUTO_INCREMENT, \`nombre\` varchar(255) NOT NULL, \`latitud\` decimal(10,7) NOT NULL, \`longitud\` decimal(10,7) NOT NULL, \`clasificacion\` enum ('principal', 'secundario', 'terminal') NOT NULL DEFAULT 'secundario', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
@@ -11,8 +11,7 @@ export class InitCinemaSchema1778004077881 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE \`ciudadanos\` (\`id\` int NOT NULL AUTO_INCREMENT, \`usuario_id\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_168b2c49c946cf96cf66ec73ff\` (\`usuario_id\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`metodos_pago\` (\`id\` int NOT NULL AUTO_INCREMENT, \`tipo\` enum ('debito', 'credito', 'recargable', 'app_movil', 'efectivo') NOT NULL DEFAULT 'debito', \`nombre\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`metodos_pago_ciudadano\` (\`id\` int NOT NULL AUTO_INCREMENT, \`numero_identificacion\` varchar(255) NOT NULL, \`activo\` tinyint NOT NULL DEFAULT 1, \`ciudadano_id\` int NULL, \`metodo_pago_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`buses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`placa\` varchar(255) NOT NULL, \`modelo\` varchar(255) NOT NULL, \`anio\` int NOT NULL, \`capacidadMaximaPasajeros\` int NOT NULL, \`capacidadSentados\` int NOT NULL, \`capacidadParados\` int NOT NULL, \`estado\` enum ('operativo', 'mantenimiento', 'fuera_de_servicio') NOT NULL DEFAULT 'operativo', \`fotoUrl\` varchar(255) NULL, \`codigoQr\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_e78e1b9df21315024e40a67d02\` (\`placa\`), UNIQUE INDEX \`IDX_44169503e1190c003aa77393f7\` (\`codigoQr\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`buses\` ADD \`disponible\` tinyint NOT NULL DEFAULT 1`);
+        await queryRunner.query(`CREATE TABLE \`buses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`placa\` varchar(255) NOT NULL, \`modelo\` varchar(255) NOT NULL, \`anio\` int NOT NULL, \`capacidadMaximaPasajeros\` int NOT NULL, \`capacidadSentados\` int NOT NULL, \`capacidadParados\` int NOT NULL, \`estado\` enum ('operativo', 'mantenimiento', 'fuera_de_servicio') NOT NULL DEFAULT 'operativo', \`fotoUrl\` longtext NULL, \`codigoQr\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_e78e1b9df21315024e40a67d02\` (\`placa\`), UNIQUE INDEX \`IDX_44169503e1190c003aa77393f7\` (\`codigoQr\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`nodos\` ADD CONSTRAINT \`FK_1db8f2a7fbf11fb7562736c37df\` FOREIGN KEY (\`ruta_id\`) REFERENCES \`rutas\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`nodos\` ADD CONSTRAINT \`FK_d0025d67eea6a1b5e32826b9f2c\` FOREIGN KEY (\`paradero_id\`) REFERENCES \`paraderos\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`direcciones\` ADD CONSTRAINT \`FK_7ac22ce345137644b3064e2eea7\` FOREIGN KEY (\`ciudadano_id\`) REFERENCES \`ciudadanos\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
@@ -26,7 +25,6 @@ export class InitCinemaSchema1778004077881 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`direcciones\` DROP FOREIGN KEY \`FK_7ac22ce345137644b3064e2eea7\``);
         await queryRunner.query(`ALTER TABLE \`nodos\` DROP FOREIGN KEY \`FK_d0025d67eea6a1b5e32826b9f2c\``);
         await queryRunner.query(`ALTER TABLE \`nodos\` DROP FOREIGN KEY \`FK_1db8f2a7fbf11fb7562736c37df\``);
-        await queryRunner.query(`ALTER TABLE \`buses\` DROP COLUMN \`disponible\``);
         await queryRunner.query(`DROP INDEX \`IDX_44169503e1190c003aa77393f7\` ON \`buses\``);
         await queryRunner.query(`DROP INDEX \`IDX_e78e1b9df21315024e40a67d02\` ON \`buses\``);
         await queryRunner.query(`DROP TABLE \`buses\``);
