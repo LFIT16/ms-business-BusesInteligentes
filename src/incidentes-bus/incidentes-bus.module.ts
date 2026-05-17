fc2205debe2472b3aa3dce1deae8e27a8d815bda
LFIT16 <ibarratucano@gmail.com>
2026-05-17 10:24:50 -0500

    Pequeños ajuste

--- src/incidentes-bus/incidentes-bus.module.ts
+++ src/incidentes-bus/incidentes-bus.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { IncidentesBusService } from './incidentes-bus.service';
 import { IncidentesBusController } from './incidentes-bus.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IncidentesBus } from './entities/incidentes-bus.entity';
+import { Bus } from '../buses/entities/bus.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      IncidentesBus,
+      Bus,
+    ]),
+  ],
   controllers: [IncidentesBusController],
   providers: [IncidentesBusService],
   exports: [IncidentesBusService],
